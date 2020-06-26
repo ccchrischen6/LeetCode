e32135e3cbc7c06d404d3925f7bb9af2b5b4ad46
ccchrischen6 <ccchrischen6@gmail.com>
2020-06-26 00:13:30 -0400

    commited on June 25th

--- src/L983_MinimumCostForTickets.java
+++ src/L983_MinimumCostForTickets.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 
 public class L983_MinimumCostForTickets {
@@ -19,4 +20,22 @@ public class L983_MinimumCostForTickets {
         }
         return dp[n];
     }
+
+//    public int mincostTickets(int[] days, int[] costs) {
+//        LinkedList<Pair<Integer,Integer>> q7 = new LinkedList();
+//        LinkedList<Pair<Integer,Integer>> q30 = new LinkedList();
+//        int total = 0;
+//
+//        for(int i=0; i<days.length; i++){
+//            int d = days[i];
+//            while(!q7.isEmpty() && q7.peek().getKey()+7 <= d) q7.poll();
+//            while(!q30.isEmpty() && q30.peek().getKey()+30 <= d) q30.poll();
+//            q7.offer(new Pair(d, total+costs[1]));
+//            q30.offer(new Pair(d, total+costs[2]));
+//            total = Math.min(total+costs[0], Math.min(
+//                    q7.peek().getValue(),
+//                    q30.peek().getValue()));
+//        }
+//        return total;
+//    }
 }
---
+++ src/Pair.java
@@ -0,0 +1,17 @@
+public class Pair<K,V> {
+    private K key;
+    private V val;
+
+    public Pair(K key, V val){
+        this.key = key;
+        this.val = val;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return val;
+    }
+}
