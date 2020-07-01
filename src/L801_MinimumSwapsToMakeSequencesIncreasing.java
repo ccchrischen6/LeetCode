063d2727a7547fdcccc9efd6bf977bb753bb7d8e
ccchrischen6 <ccchrischen6@gmail.com>
2020-07-01 15:56:16 -0400

    commited on July 1st

---
+++ src/L801_MinimumSwapsToMakeSequencesIncreasing.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class L801_MinimumSwapsToMakeSequencesIncreasing {
+    public int minSwap(int[] A, int[] B) {
+        int res = 0;
+        int n = A.length;
+        int[] keep = new int[n];
+        int[] swap = new int[n];
+        Arrays.fill(keep, Integer.MAX_VALUE);
+        Arrays.fill(swap, Integer.MAX_VALUE);
+        keep[0] = 0;
+        swap[0] = 1;
+
+        for(int i=1; i<n; i++){
+            if(A[i] > A[i-1] && B[i] > B[i-1]) {
+                keep[i] = keep[i-1];
+                swap[i] = swap[i-1]+1;
+            }
+
+            if(A[i] > B[i-1] && B[i] > A[i-1]){
+                keep[i] = Math.min(keep[i], swap[i-1]);
+                swap[i] = Math.min(swap[i], keep[i-1]+1);
+            }
+        }
+        return Math.min(keep[n-1], swap[n-1]);
+    }
+}
