0593d65fb67669d41d07ebfd33ad4ea08b595389
ccchrischen6 <ccchrischen6@gmail.com>
2020-07-01 00:18:15 -0400

    commited on June 30th

--- src/L474_Ones_and_Zeroes.java
+++ src/L474_Ones_and_Zeroes.java
@@ -30,4 +30,61 @@ public class L474_Ones_and_Zeroes {
         }
         return res;
     }
+
+    //three dimensional array
+//    public int findMaxForm(String[] strs, int m, int n) {
+//        int len = strs.length;
+//        int[][][] dp = new int[len+1][m+1][n+1];
+//        for(int i=1; i<=len; i++){
+//            int[] count = getCount(strs[i-1]);
+//            for(int j=0; j<=m; j++){
+//                for(int k=0; k<=n; k++){
+//                    if(j >= count[0] && k >= count[1]){
+//                        dp[i][j][k] = Math.max(dp[i-1][j][k], 1 + dp[i-1][j-count[0]][k-count[1]]);
+//                    }
+//                    else dp[i][j][k] = dp[i-1][j][k];
+//                }
+//            }
+//        }
+//        return dp[len][m][n];
+//    }
+//
+//    private int[] getCount(String s){
+//        int[] res = new int[2];
+//        for(int i=0; i<s.length(); i++){
+//            if(s.charAt(i) == '0') res[0]++;
+//            else res[1]++;
+//        }
+//        return res;
+//    }
+
+    //two dimensional array
+//    public int findMaxForm(String[] strs, int m, int n) {
+//        int len = strs.length;
+//        int[][] dp = new int[m+1][n+1];
+//        for(String i : strs){
+//            int[] count = getCount(i);
+//            int[][] cur = new int[m+1][n+1];
+//
+//            for(int j=0; j<=m; j++){
+//                for(int k=0; k<=n; k++){
+//                    if(j >= count[0] && k >= count[1]){
+//                        cur[j][k] = Math.max(dp[j][k], 1 + dp[j-count[0]][k-count[1]]);
+//                    }
+//                    else cur[j][k] = dp[j][k];
+//                }
+//            }
+//            dp = cur;
+//        }
+//        return dp[m][n];
+//    }
+//
+//    private int[] getCount(String s){
+//        int[] res = new int[2];
+//        for(int i=0; i<s.length(); i++){
+//            if(s.charAt(i) == '0') res[0]++;
+//            else res[1]++;
+//        }
+//        return res;
+//    }
 }
